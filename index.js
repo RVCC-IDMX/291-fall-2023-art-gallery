4010c24d6f211e87f8fddefc18f5fdc2376a6bec
Eric Brown <eric.brown8787@gmail.com>
2023-11-22 00:34:34 -0500

    Add sample images and text to artist bio

--- index.js
+++ index.js
@@ -1,3 +1,2 @@
-import * as PIXI from "https://cdn.jsdelivr.net/npm/pixi.js@7.3.2/dist/pixi.min.js";
-
-console.log("This works!");
+// import * as PIXI from "https://cdn.jsdelivr.net/npm/pixi.js@7.3.2/dist/pixi.min.js";
+// import artists from "./artists.json";
