0f40ff5f97a0d681b5cc333c49e6cf9c668e0040
Eric Brown <eric.brown8787@gmail.com>
2023-12-05 21:03:01 -0500

    Add more varied artist data

---
+++ data.js
@@ -0,0 +1,148 @@
+const artistData = [
+  {
+    name: "Isabella Rivera",
+    location: "New York City, USA",
+    medium: "Oil painting",
+    description:
+      "    Isabella Rivera's art is an exploration of the juxtaposition between the urban and the natural world. In the heart of New York City, she finds inspiration in the city's towering skyscrapers and bustling streets, which she blends seamlessly with the serene beauty of nature. \n    Her oil paintings aim to capture the essence of this harmony by using vivid colors and intricate details to depict the dynamic interplay between these two seemingly contrasting realms. Through her work, she hopes to remind viewers that even within the concrete jungle, there is a place for the organic and the tranquil.",
+    headshotPath: "./img/headshots/headshot.jpg",
+    galleryImages: [
+      "./img/gallery/mountains.jpg",
+      "./img/gallery/prism.jpg",
+      "./img/gallery/wind.jpg",
+      "./img/gallery/dog.jpg",
+      "./img/gallery/trees.jpg",
+      "./img/gallery/abstract.jpg",
+    ],
+  },
+  {
+    name: "Felix Chen",
+    location: "Tokyo, Japan",
+    medium: "Digital sculpture",
+    description:
+      "Felix Chen's digital sculptures are an homage to the synthesis of tradition and innovation, deeply rooted in Tokyo's culture. He sculpts intricate, ethereal forms that embody the intricate relationship between technology and nature, seamlessly blending ancient Japanese aesthetics with futuristic elements. With each piece, he aims to challenge our understanding of beauty and art, reminding us that the fusion of the old and the new can result in astonishing and harmonious creations.",
+    headshotPath: "./img/headshots/headshot.jpg",
+    galleryImages: [
+      "./img/gallery/mountains.jpg",
+      "./img/gallery/prism.jpg",
+      "./img/gallery/wind.jpg",
+      "./img/gallery/dog.jpg",
+      "./img/gallery/trees.jpg",
+      "./img/gallery/abstract.jpg",
+    ],
+  },
+  {
+    name: "Aurora Silva",
+    location: "Reykjavik, Iceland",
+    medium: "Photography",
+    description:
+      "Aurora Silva's stunning landscapes are a constant source of inspiration for her photography. Her work seeks to uncover the mystical beauty hidden within Iceland's rugged and pristine environment, capturing the unique balance between desolation and magnificence. By focusing on the interplay of light, shadow, and texture, she strives to expose the vulnerability of nature and the profound impact of human existence on these remote lands. Through her lens, she hopes to convey a sense of awe and responsibility for the preservation of our planet's natural wonders.",
+    headshotPath: "./img/headshots/headshot.jpg",
+    galleryImages: [
+      "./img/gallery/mountains.jpg",
+      "./img/gallery/prism.jpg",
+      "./img/gallery/wind.jpg",
+      "./img/gallery/dog.jpg",
+      "./img/gallery/trees.jpg",
+      "./img/gallery/abstract.jpg",
+    ],
+  },
+  {
+    name: "Hector Ramos",
+    location: "Barcelona, Spain",
+    medium: "Ceramics",
+    description:
+      "Hector Ramos' art is a celebration of imperfection and the beauty found in flaws. In the heart of Barcelona, he molds clay into unique forms that embrace the irregularities of the handmade process. He firmly believes that it's the imperfections that make each piece truly special. His ceramics are characterized by their organic shapes, raw textures, and earthy glazes. He wants viewers to connect with the authenticity of the materials and appreciate the essence of the human touch in every piece, which tells a story of the maker and the journey of creation.",
+    headshotPath: "./img/headshots/headshot.jpg",
+    galleryImages: [
+      "./img/gallery/mountains.jpg",
+      "./img/gallery/prism.jpg",
+      "./img/gallery/wind.jpg",
+      "./img/gallery/dog.jpg",
+      "./img/gallery/trees.jpg",
+      "./img/gallery/abstract.jpg",
+    ],
+  },
+  {
+    name: "Eliza Wu",
+    location: "Melbourne, Australia",
+    medium: "Street art",
+    description:
+      "Eliza Wu's laneways and walls of Melbourne are her canvas, and the city's hidden secrets are her muse. Her street art brings life to the urban environment, offering a visual narrative that blurs the line between the mundane and the extraordinary. Through vibrant colors, intricate stencils, and evocative imagery, she aims to transform public spaces into avenues of imagination and self-expression. Her work represents the voice of the city, reflecting the diversity, creativity, and raw energy that Melbourne exudes.",
+    headshotPath: "./img/headshots/headshot.jpg",
+    galleryImages: [
+      "./img/gallery/mountains.jpg",
+      "./img/gallery/prism.jpg",
+      "./img/gallery/wind.jpg",
+      "./img/gallery/dog.jpg",
+      "./img/gallery/trees.jpg",
+      "./img/gallery/abstract.jpg",
+    ],
+  },
+  {
+    name: "Lila Khan",
+    location: "Marrakech, Morocco",
+    medium: "Textile design",
+    description:
+      "Lila Khan's textiles are a bridge between the past and the present, interweaving the traditional patterns of Morocco with a contemporary twist. Marrakech's rich cultural heritage serves as the foundation for her designs, where ancient motifs and vibrant colors come alive in modern contexts. Each piece of fabric tells a story of Morocco's history, from the intricate tilework of palaces to the bustling souks and the rugged landscapes of the Atlas Mountains. Through her work, she aims to share the timeless beauty of Moroccan culture and foster an appreciation for its exquisite craftsmanship.",
+    headshotPath: "./img/headshots/headshot.jpg",
+    galleryImages: [
+      "./img/gallery/mountains.jpg",
+      "./img/gallery/prism.jpg",
+      "./img/gallery/wind.jpg",
+      "./img/gallery/dog.jpg",
+      "./img/gallery/trees.jpg",
+      "./img/gallery/abstract.jpg",
+    ],
+  },
+  {
+    name: "Maxime Dupont",
+    location: "Paris, France",
+    medium: "Installation art",
+    description:
+      "Maxime Dupont's art seeks to provoke introspection and challenge perceptions. Paris, with its deep history and complex layers of time, serves as a perfect canvas for his immersive installations. He creates spaces that invite viewers to question their relationship with space and time, altering their perspectives on reality. His installations use a mix of multimedia elements, light, sound, and physical structures to immerse viewers in a sensory journey. By offering alternative realities and novel perspectives, he hopes to spark conversations about the fluid and ever-evolving nature of our existence.",
+    headshotPath: "./img/headshots/headshot.jpg",
+    galleryImages: [
+      "./img/gallery/mountains.jpg",
+      "./img/gallery/prism.jpg",
+      "./img/gallery/wind.jpg",
+      "./img/gallery/dog.jpg",
+      "./img/gallery/trees.jpg",
+      "./img/gallery/abstract.jpg",
+    ],
+  },
+
+  {
+    name: "Isabella Rivera",
+    location: "New York City, USA",
+    medium: "Oil painting",
+    description:
+      "    Isabella Rivera's art is an exploration of the juxtaposition between the urban and the natural world. In the heart of New York City, she finds inspiration in the city's towering skyscrapers and bustling streets, which she blends seamlessly with the serene beauty of nature. \n    Her oil paintings aim to capture the essence of this harmony by using vivid colors and intricate details to depict the dynamic interplay between these two seemingly contrasting realms. Through her work, she hopes to remind viewers that even within the concrete jungle, there is a place for the organic and the tranquil.",
+    headshotPath: "./img/headshots/headshot.jpg",
+    galleryImages: [
+      "./img/gallery/mountains.jpg",
+      "./img/gallery/prism.jpg",
+      "./img/gallery/wind.jpg",
+      "./img/gallery/dog.jpg",
+      "./img/gallery/trees.jpg",
+      "./img/gallery/abstract.jpg",
+    ],
+  },
+
+  {
+    name: "Isabella Rivera",
+    location: "New York City, USA",
+    medium: "Oil painting",
+    description:
+      "    Isabella Rivera's art is an exploration of the juxtaposition between the urban and the natural world. In the heart of New York City, she finds inspiration in the city's towering skyscrapers and bustling streets, which she blends seamlessly with the serene beauty of nature. \n    Her oil paintings aim to capture the essence of this harmony by using vivid colors and intricate details to depict the dynamic interplay between these two seemingly contrasting realms. Through her work, she hopes to remind viewers that even within the concrete jungle, there is a place for the organic and the tranquil.",
+    headshotPath: "./img/headshots/headshot.jpg",
+    galleryImages: [
+      "./img/gallery/mountains.jpg",
+      "./img/gallery/prism.jpg",
+      "./img/gallery/wind.jpg",
+      "./img/gallery/dog.jpg",
+      "./img/gallery/trees.jpg",
+      "./img/gallery/abstract.jpg",
+    ],
+  },
+];
