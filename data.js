6a5712301426af4f2b1cf86069af14a87d421fec
Eric Brown <eric.brown8787@gmail.com>
2023-12-05 22:36:29 -0500

    Re-add artist photos to data objects

--- data.js
+++ data.js
@@ -1,3 +1,10 @@
+const exhibitData = [
+  {
+    title: "title",
+    description: "description",
+  },
+];
+
 const artistData = [
   {
     name: "Isabella Rivera",
@@ -21,7 +28,7 @@ const artistData = [
     medium: "Digital sculpture",
     description:
       "Felix Chen's digital sculptures are an homage to the synthesis of tradition and innovation, deeply rooted in Tokyo's culture. He sculpts intricate, ethereal forms that embody the intricate relationship between technology and nature, seamlessly blending ancient Japanese aesthetics with futuristic elements. With each piece, he aims to challenge our understanding of beauty and art, reminding us that the fusion of the old and the new can result in astonishing and harmonious creations.",
-    headshotPath: "./img/headshots/headshot.jpg",
+    headshotPath: "./img/headshots/man2.jpg",
     galleryImages: [
       "./img/gallery/mountains.jpg",
       "./img/gallery/prism.jpg",
@@ -37,7 +44,7 @@ const artistData = [
     medium: "Photography",
     description:
       "Aurora Silva's stunning landscapes are a constant source of inspiration for her photography. Her work seeks to uncover the mystical beauty hidden within Iceland's rugged and pristine environment, capturing the unique balance between desolation and magnificence. By focusing on the interplay of light, shadow, and texture, she strives to expose the vulnerability of nature and the profound impact of human existence on these remote lands. Through her lens, she hopes to convey a sense of awe and responsibility for the preservation of our planet's natural wonders.",
-    headshotPath: "./img/headshots/headshot.jpg",
+    headshotPath: "./img/headshots/woman3.jpg",
     galleryImages: [
       "./img/gallery/mountains.jpg",
       "./img/gallery/prism.jpg",
@@ -53,7 +60,7 @@ const artistData = [
     medium: "Ceramics",
     description:
       "Hector Ramos' art is a celebration of imperfection and the beauty found in flaws. In the heart of Barcelona, he molds clay into unique forms that embrace the irregularities of the handmade process. He firmly believes that it's the imperfections that make each piece truly special. His ceramics are characterized by their organic shapes, raw textures, and earthy glazes. He wants viewers to connect with the authenticity of the materials and appreciate the essence of the human touch in every piece, which tells a story of the maker and the journey of creation.",
-    headshotPath: "./img/headshots/headshot.jpg",
+    headshotPath: "./img/headshots/man3.jpg",
     galleryImages: [
       "./img/gallery/mountains.jpg",
       "./img/gallery/prism.jpg",
@@ -69,7 +76,7 @@ const artistData = [
     medium: "Street art",
     description:
       "Eliza Wu's laneways and walls of Melbourne are her canvas, and the city's hidden secrets are her muse. Her street art brings life to the urban environment, offering a visual narrative that blurs the line between the mundane and the extraordinary. Through vibrant colors, intricate stencils, and evocative imagery, she aims to transform public spaces into avenues of imagination and self-expression. Her work represents the voice of the city, reflecting the diversity, creativity, and raw energy that Melbourne exudes.",
-    headshotPath: "./img/headshots/headshot.jpg",
+    headshotPath: "./img/headshots/woman4.jpg",
     galleryImages: [
       "./img/gallery/mountains.jpg",
       "./img/gallery/prism.jpg",
@@ -85,7 +92,7 @@ const artistData = [
     medium: "Textile design",
     description:
       "Lila Khan's textiles are a bridge between the past and the present, interweaving the traditional patterns of Morocco with a contemporary twist. Marrakech's rich cultural heritage serves as the foundation for her designs, where ancient motifs and vibrant colors come alive in modern contexts. Each piece of fabric tells a story of Morocco's history, from the intricate tilework of palaces to the bustling souks and the rugged landscapes of the Atlas Mountains. Through her work, she aims to share the timeless beauty of Moroccan culture and foster an appreciation for its exquisite craftsmanship.",
-    headshotPath: "./img/headshots/headshot.jpg",
+    headshotPath: "./img/headshots/woman2.jpg",
     galleryImages: [
       "./img/gallery/mountains.jpg",
       "./img/gallery/prism.jpg",
@@ -101,7 +108,7 @@ const artistData = [
     medium: "Installation art",
     description:
       "Maxime Dupont's art seeks to provoke introspection and challenge perceptions. Paris, with its deep history and complex layers of time, serves as a perfect canvas for his immersive installations. He creates spaces that invite viewers to question their relationship with space and time, altering their perspectives on reality. His installations use a mix of multimedia elements, light, sound, and physical structures to immerse viewers in a sensory journey. By offering alternative realities and novel perspectives, he hopes to spark conversations about the fluid and ever-evolving nature of our existence.",
-    headshotPath: "./img/headshots/headshot.jpg",
+    headshotPath: "./img/headshots/man1.jpg",
     galleryImages: [
       "./img/gallery/mountains.jpg",
       "./img/gallery/prism.jpg",
@@ -118,7 +125,7 @@ const artistData = [
     medium: "Woodworking",
     description:
       "Aiden Foster's woodworking is an ode to the resilience of nature and the beauty of reclamation. In the diverse landscapes of Cape Town, he uncovers the hidden stories within reclaimed wood and transforms them into functional pieces of art. Each piece he creates carries the history of the materials and the land they come from, reminding us of the importance of sustainability and the potential for renewal. His goal is to breathe new life into the discarded and to remind us of the significance of cherishing our planet's resources.",
-    headshotPath: "./img/headshots/headshot.jpg",
+    headshotPath: "./img/headshots/man4.jpg",
     galleryImages: [
       "./img/gallery/mountains.jpg",
       "./img/gallery/prism.jpg",
@@ -134,7 +141,7 @@ const artistData = [
     medium: "Digital collage",
     description:
       "Maya Patel's digital collages are a cultural tapestry that weaves together elements from India and around the world. Mumbai's vibrant and diverse atmosphere serves as a constant source of inspiration for her work. Through layers of imagery, she blends elements of tradition and modernity, fusing colors, patterns, and stories to create visual narratives that speak to the diversity of humanity. Her collages are a celebration of the global connections that bridge cultures, languages, and experiences, reminding us of the beauty in our shared human tapestry.",
-    headshotPath: "./img/headshots/headshot.jpg",
+    headshotPath: "./img/headshots/woman3.jpg",
     galleryImages: [
       "./img/gallery/mountains.jpg",
       "./img/gallery/prism.jpg",
